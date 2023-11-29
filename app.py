3ba7dc206cb726cbaa69506ff0adc6160e65caee
Jonathan <jonathanmoren3@gmail.com>
2023-11-29 16:35:11 -0600

    Second commit

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-# First Commit
-print("Hello world")
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(
+    debug = True
+)
 
 
 
